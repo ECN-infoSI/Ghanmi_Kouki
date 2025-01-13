4d007bedbfaff58988edc599278307535dc347f8
DhaferG <dhaferghanmi.dg13@gmail.com>
2025-01-13 08:40:57 +0100

    Added attributes and constructor to class Player

--- Ghanmi_Kouki/src/main/java/nour/ghanmi_kouki/Player.java
+++ Ghanmi_Kouki/src/main/java/nour/ghanmi_kouki/Player.java
@@ -1,5 +1,21 @@
 package nour.ghanmi_kouki;
 
+/*
+ * Class to represent the players of the game
+ */
 public class Player {
+    private String name; //name of the player
+    private Pawn.Color color; //Enum defined in class Pawn
+    /*
+     * Constructor of the class player
+     * 
+     * @param name: Name of the player
+     * @param color: color of the player's pawns
+     */
+    public Player(String name, Pawn.color color){
+        this.name= name;
+        this.color = color;
+    }
+
 
 }
