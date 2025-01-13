52ba378f1e6aa10661a89c710f5bf5b1d3c51c81
nekouki <nour-elhouda.kouki@eleves.ec-nantes.fr>
2025-01-13 09:29:58 +0100

    methode initilialiser plateau qui initialise les cases vides dans le plateau ainsi que les 4 positions de depart

--- Ghanmi_Kouki/src/main/java/nour/ghanmi_kouki/Plateau.java
+++ Ghanmi_Kouki/src/main/java/nour/ghanmi_kouki/Plateau.java
@@ -19,9 +19,24 @@ public class Plateau {
      */
     public Plateau() {
         grille = new Pion[TAILLE][TAILLE];
+        initialiserPlateau();
+    }
+    
+    /**
+     * Initialise le plateau avec des pions vides et les positions de départ.
+     */
+    private void initialiserPlateau() {
+        for (int i = 0; i < TAILLE; i++) {
+            for (int j = 0; j < TAILLE; j++) {
+                grille[i][j] = new Pion(Pion.Couleur.VIDE);
+            }
+        }
+        grille[3][3].setCouleur(Pion.Couleur.BLANC);
+        grille[3][4].setCouleur(Pion.Couleur.NOIR);
+        grille[4][3].setCouleur(Pion.Couleur.NOIR);
+        grille[4][4].setCouleur(Pion.Couleur.BLANC);
     }
    
-
     
     
 }
