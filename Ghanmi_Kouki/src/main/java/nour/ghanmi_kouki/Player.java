b1538b2830a053a1e3217f81d8f3cbb94235f858
nekouki <nour-elhouda.kouki@eleves.ec-nantes.fr>
2025-01-13 11:00:06 +0100

    resolving merge conflicts classe player

--- Ghanmi_Kouki/src/main/java/nour/ghanmi_kouki/Player.java
+++ Ghanmi_Kouki/src/main/java/nour/ghanmi_kouki/Player.java
@@ -1,13 +1,13 @@
 package nour.ghanmi_kouki;
 
 /**
- * Classe représentant les joueurs
+ * Classe représntnat les joueurs
  */
 public class Player {
     private String name; //Nom du joueur
     private Pion.Couleur color; //couleur des pions du joueur
     /**
-     * Constructeur de la classe Player
+     * Constructeru de la classe Player
      * 
      * @param name: Name of the player
      * @param color: color of the player's pawns
@@ -31,7 +31,7 @@ public class Player {
      * @return couleur des pièces du joueurs.
      */
     public Pion.Couleur getCouleur() {
-        return color;
+        return this.color;
     }
 
     
